7778264b4bcd01f7612b82898d17bc83ac734678
Luis Corales <lcorales@stackbuilders.com>
2022-07-18 15:04:47 -0500

    Setup testing correctly

--- src/views/Home.test.tsx
+++ src/views/Home.test.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { render, fireEvent, waitFor, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import Home from "./Home";
 
-describe("first", () => {
-  test("should first", () => {
-    expect(true).toBe(true);
+describe("Home view", () => {
+  test("renders the view", async () => {
+    render(<Home />);
+
+    expect(await screen.findByText("Home")).toHaveTextContent("Home");
   });
-});
-
-test("should first", () => {
-  expect(true).toBe(true);
 });

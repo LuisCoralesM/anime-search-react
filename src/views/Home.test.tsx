14c0ae328c5119fc906efb82a4b3702e079a8840
Luis Corales <lcorales@stackbuilders.com>
2022-07-18 10:14:09 -0500

    Add testing framework

---
+++ src/views/Home.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+describe("first", () => {
+  test("should first", () => {
+    expect(true).toBe(true);
+  });
+});
+
+test("should first", () => {
+  expect(true).toBe(true);
+});
